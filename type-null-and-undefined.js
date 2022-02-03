9a98a8a739cb4725fbd5caf6aa387f9b7adcc286
Caio Pellegrini <caiopellegrini1997@gmail.com>
2022-02-03 20:31:12 -0300

    types null and undefined

---
+++ type-null-and-undefined.js
@@ -0,0 +1,16 @@
+
+
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+let input1 = null;
+let input2;
+
+console.log(input1);
+console.log(input2);

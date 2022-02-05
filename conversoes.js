5ecd13996ccad5fdd1519b5a4e182dc5b9e61382
Caio Pellegrini <caiopellegrini1997@gmail.com>
2022-02-04 21:10:10 -0300

    conversões

--- conversoes.js
+++ conversoes.js
@@ -20,5 +20,22 @@ const numero = 456;
 const numeroString = Number("456")
 console.log(numero + numeroString)
 
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString());
+
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura);
+
+let usuarioConectado = false;
+console.log(Number(usuarioConectado));
+
+let usuarioConectado2 = true;
+console.log(Number(usuarioConectado2));
+
+
+
+
+
 
 

8426a063f9112acbc07c2fb6a8a6bf0c23aadc9a
Caio Pellegrini <caiopellegrini1997@gmail.com>
2022-02-04 21:04:51 -0300

    conversões

---
+++ conversoes.js
@@ -0,0 +1,24 @@
+// tipo de dado
+// booleanos
+
+//conversão implícita (pode trazer alguns bugs)
+
+const numero = 456;
+// const numeroString = "456";
+
+// console.log(numero === numeroString);
+// console.log(numero == numeroString);
+
+// console.log(numero + numeroString);
+
+// conversão explícita
+// Number()
+// String()
+
+// console.log(numero + Number(numeroString))
+
+const numeroString = Number("456")
+console.log(numero + numeroString)
+
+
+

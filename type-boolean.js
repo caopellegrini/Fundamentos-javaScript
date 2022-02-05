72d44801f48b060ae924d7fdfb45958c5566dde7
Caio Pellegrini <caiopellegrini1997@gmail.com>
2022-02-04 21:02:44 -0300

    type

--- type-boolean.js
+++ type-boolean.js
@@ -1,11 +1,44 @@
 // true or false
 
-const num1 = 1;
-const num2 = 5;
-let cadastroAtivado = true;
+// const num1 = 1;
+// const num2 = 5;
+// let cadastroAtivado = true;
 
-const text1 = "Alura";
-const text2 = "alura";
+// const text1 = "Alura";
+// const text2 = "alura";
 
-console.log(num1 === num2);
-console.log(text1 === text2);
+// console.log(num1 === num2);
+// console.log(text1 === text2);
+
+// boolean 
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy (tipo verdadeiro ou tipo falso)
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// obs: == (coprar só o valor) === (comprar tipo e valor)
+
+//null
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let string = "Alura";
+
+console.log(typeof numero)
+console.log(typeof string)
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+
+// é um erro null === objeto na vdd ele deveria ser do tipo null

2726e6bbe4077ab6ecd78b3879365c95ec525760
Caio Pellegrini <caiopellegrini1997@gmail.com>
2022-02-09 21:31:57 -0300

    Error and comparison

--- comparacoes.js
+++ comparacoes.js
@@ -16,19 +16,3 @@ console.log(typeof texto);
 
 // ainda tem códigos que utilizam dois iguais, mas como uma prática é ideal usar os três iguais
 
-// operador tenário
-
-const idadeMinima = 18;
-const idadeCliente = 16;
-
-// if (idadeCliente >= idadeMinima){
-//     console.log("Cerveja")
-// } else{
-//     console.log("suco")
-// }
-
-console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
-
-// não é recomendado usar tenário dentro de tenário pois dificulta a leitura
-
-
--- erros.js
+++ erros.js
@@ -4,4 +4,4 @@ const numeroSemErro = 2;
 console.log(minhaVar);
 
 // (D:\caminho até o arquivo\arqiuvo:4:13) A última parte é linha e o caracter que começa o erro
-// Outras coisas listadas é o caminho de execução da aplicação com todas a ferramentas e bibliotecas envolvidas
+// Outras coisas listadas é o caminho de execução da aplicação com todas a ferramentas e bibliotecas envolvidas e é chamado de stack trace
